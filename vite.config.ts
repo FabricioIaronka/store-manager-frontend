4797028da2bf2471427ff33b18906e5db6e4157c
FabricioIaronka <fabricioiaronka1@gmail.com>
2025-12-17 20:47:57 -0300

    Configs to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/store-manager-frontend",
   css: {
     preprocessorOptions: {
       scss: {
